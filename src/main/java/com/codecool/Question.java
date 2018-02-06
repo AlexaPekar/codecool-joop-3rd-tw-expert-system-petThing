490da979363ecfa475754cad51d5430e2d443128
AlexaPekar <missdreambelle@gmail.com>
2018-02-06 15:05:18 +0100

    Implemented fields and getters

--- src/main/java/com/codecool/Question.java
+++ src/main/java/com/codecool/Question.java
@@ -2,20 +2,25 @@ package com.codecool;
 
 public class Question {
 
+    String id;
+    String question;
+    Answer answer;
+
+
     public Question(String id, String question, Answer answer) {
 
     }
 
     public String getId() {
-
+        return id;
     }
 
     public String getQuestion() {
-
+        return question;
     }
 
     public Answer getAnswer() {
-
+        return answer;
     }
 
     public boolean getEvalutedAnswer(String input) {
