43629bc98040877bfa3ff824cc50af2ee1f3dd20
norcs <norbertcs@me.com>
2018-02-06 13:45:56 +0100

    Add getValueById() and setFactValueById

--- src/main/java/com/codecool/Fact.java
+++ src/main/java/com/codecool/Fact.java
@@ -3,20 +3,55 @@ package com.codecool;
 import java.util.Set;
 
 public class Fact {
+    
+    private String id;
+    private String description;
+    private Set<String> idSet;
+    private boolean livingSpace;
+    private boolean freetime;
+    private boolean money;
+    private boolean activeness;
+    private boolean cleaning;
 
     public Fact(String id, String description) {
-
+        this.id = id;
+        this.description = description;
     }
 
     public Set<String> getIdSet() {
+        return idSet;
+    }
 
+    public boolean getValueById(String id) {
+        if (id.equalsIgnoreCase("livingSpace")) {
+            return livingSpace;
+        } if (id.equalsIgnoreCase("freetime")) {
+            return freetime;
+        } if (id.equalsIgnoreCase("money")) {
+            return money;
+        } if (id.equalsIgnoreCase("activeness")) {
+            return activeness;
+        } if (id.equalsIgnoreCase("cleaning")) {
+            return cleaning;
+        }
+        return Boolean.parseBoolean(null);
     }
 
     public void setFactValueById(String id, boolean value) {
-
+        if (id.equalsIgnoreCase("livingSpace")) {
+            livingSpace = value;
+        } if (id.equalsIgnoreCase("freetime")) {
+            freetime = value;
+        } if (id.equalsIgnoreCase("money")) {
+            money = value;
+        } if (id.equalsIgnoreCase("activeness")) {
+            activeness = value;
+        } if (id.equalsIgnoreCase("cleaning")) {
+            cleaning = value;
+        }
     }
 
     public String getDescription() {
-
+        return description;
     }
 }
