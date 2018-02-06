a019fafd303bc8b60203d639728d196d25b28f7c
norcs <norbertcs@me.com>
2018-02-06 14:35:44 +0100

    Add addFact() and getIterator()

--- src/main/java/com/codecool/FactRepository.java
+++ src/main/java/com/codecool/FactRepository.java
@@ -1,15 +1,21 @@
 package com.codecool;
 
+import java.util.Iterator;
+import java.util.List;
+
 public class FactRepository {
 
+    private List<Fact> facts;
+    private Iterator<Fact> fact;
+
     public FactRepository() {
+    }
 
-        public void addFact(Fact fact) {
+    public void addFact(Fact fact) {
+        facts.add(fact);
+    }
 
-        }
-
-        public Iterator<Fact> getIterator() {
-
-        }
+    public Iterator<Fact> getIterator() {
+        return fact;
     }
 }
