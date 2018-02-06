79e0168340570240b6aece00ee8077cf94d42b1e
hreskonorbert <norberthresko@gmail.com>
2018-02-06 15:02:24 +0100

    Created skeleton for factiterator

---
+++ src/main/java/com/codecool/FactIterator.java
@@ -0,0 +1,17 @@
+package com.codecool;
+
+import java.util.Iterator;
+
+public class FactIterator implements Iterator<Fact> {
+
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Fact next() {
+        return null;
+    }
+}
