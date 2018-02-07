2a96418f083f605d8d6e984e0eb3bf9beb8b19a3
Bálint Csizmadia <balintcsizmadia1@gmail.com>
2018-02-07 13:48:28 +0100

    Implements Answer class

--- src/main/java/com/codecool/Answer.java
+++ src/main/java/com/codecool/Answer.java
@@ -1,16 +1,28 @@
 package com.codecool;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Answer {
 
-    public Answer() {
+    Value value;
+    List<Value> values = new ArrayList<>();
 
+
+    public Answer() {
     }
 
-    public boolean evaluateAnswerByInput(String input) {
-                                return false;
+    public boolean evaluateAnswerByInput(String input) throws Exception {
+        boolean result = false;
+        for (int i = 0; i < values.size(); i++) {
+            if (values.get(i).getParam().equals(input)) {
+                result = true;
+            }
+        }
+        return result;
     }
 
     public void addValue(Value value) {
-
+        values.add(value);
     }
 }
