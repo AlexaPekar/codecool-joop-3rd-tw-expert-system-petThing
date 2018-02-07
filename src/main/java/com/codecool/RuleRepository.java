e7149946639a280cf7f03ac7f3ecf639eced75dc
AlexaPekar <missdreambelle@gmail.com>
2018-02-07 11:16:00 +0100

    Implement RuleRepository's fields and methods

--- src/main/java/com/codecool/RuleRepository.java
+++ src/main/java/com/codecool/RuleRepository.java
@@ -1,16 +1,27 @@
 package com.codecool;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class RuleRepository {
 
-    public RuleRepository() {
+    private List<Question> questions = new ArrayList<>();
+    private Iterator<Question> questionIter;
 
+    public RuleRepository(QuestionIterator questionIter) {
+        this.questionIter = questionIter;
     }
 
     public void addQuestion(Question question) {
-
+        questions.add(question);
     }
 
-    /*public Iterator<Question> getIterator() {
+    public Iterator<Question> getIterator() {
+        return questionIter;
+    }
 
-    }*/
+    public List<Question> getQuestions() {
+        return questions;
+    }
 }
