146f3a6aa2462cb90f2ae05a7f03385bcbd80196
AlexaPekar <missdreambelle@gmail.com>
2018-02-07 11:16:29 +0100

    Created QuestionIterator class

---
+++ src/main/java/com/codecool/QuestionIterator.java
@@ -0,0 +1,17 @@
+package com.codecool;
+
+import java.util.Iterator;
+
+public class QuestionIterator implements Iterator<Question> {
+
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Question next() {
+        return null;
+    }
+}
